8870a215e5bee18c5e9eeb4bb8a6859a7c3a1095
Mattia Ziglioli <mtt.zgl@gmail.com>
2025-07-08 00:32:52 +0200

    added data reshaping function to test data in utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def resize_data(L, lt, ut, t_step):
+def resize_data(L, lt, ut):
 
     df = pd.read_csv(f"./data_generation/data/{L}/s3_cfg_L{L}_A0_mc100000_burn1_tl{lt:.3f}_tu{ut:.3f}.csv", header = None)
     t = df[L]# last column of dataframe, corresponding to temperatures
@@ -28,3 +28,51 @@ def resize_data(L, lt, ut, t_step):
     np.save(f"data/{L}", data)
     np.save(f"data/{L}_temp", temp)
 
+
+def resize_data_test(L):
+    '''
+    Resize test data. Metropolis algorithm is run 10 time on the same temperatures
+    and intervals in order to get statistical quantities out of the data.
+    In L_test there are 10 simulations concatenated: the function divide the file in 10 pieces and each of the pieces is resized like in resize_data()
+    '''  
+    
+    N = 10 # times algorithm has been run
+    a = []
+    for i in range(N):
+        dff =  pd.read_csv(f"./data_generation/data/tmp/{i}.csv", header = None)
+        a.append(dff)
+
+
+    df = pd.concat(a)
+    t = df[L]# last column of dataframe, corresponding to temperatures
+    df.drop(labels=L, axis=1, inplace=True)
+
+    
+
+    # Creating numpy array where each element is a lattice configuration
+    # c++ code creates (L*temperatures, L) csv file
+
+    # n = configurations generated at different temperatures
+    n = int(df.shape[0] / L / N) 
+    # data where each row is data-point and there are L*L columns corresponding to the whole lattice
+    data = np.zeros((N,n, L*L))
+    temp = np.zeros((N,n))
+    
+    kk = 0 # index for filling data again
+    for j in range(0, N*n*L, n*L):
+        k=0
+        w = df.iloc[j:j+n*L] # select first configuration
+        y = t[j:j+n*L]
+        # n*L rows, step=L every L row a new configuration starts
+        for i in range(0,n*L, L):
+            z = w.iloc[i:i+L].to_numpy()
+            data[kk, k, :] = z.flatten()
+            temp[kk, k] = y[i]
+            k += 1
+        
+        kk += 1
+    
+    np.save(f"data/{L}_test", data)
+    np.save(f"data/{L}_test_temp", temp)
+
+resize_data_test(20)
