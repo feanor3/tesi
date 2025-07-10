a1e1108fc72c2623b4ac0049a2ec99c6a93061e4
Mattia Ziglioli <mtt.zgl@gmail.com>
2025-07-10 16:19:54 +0200

    added sgd to classifier, momentum is missing

--- classifier.py
+++ classifier.py
@@ -30,17 +30,30 @@ def add_bias(X, bias):
 class MLPBinary():
     """A multi-layer neural network with one hidden layer"""
     
-    def __init__(self, bias=-1, dim_hidden = 6, tolerance=1e-4, activation='relu'):
+    def __init__(self, bias=-1, dim_hidden = 6, tolerance=1e-4, activation='relu', solver='sgd', lr = 1e-4, batch_size=100, epochs=100, n_epochs_no_update = 2, momentum = 0.9):
         """Intialize the hyperparameters"""
         self.bias = bias
         # Dimensionality of the hidden layer
         self.dim_hidden = dim_hidden
         self.epochs = 0
         self.tol = tolerance
+        self.lr = lr
+        self.epochs = epochs
+        self.n_epochs_no_update = n_epochs_no_update
+        self.solver = solver
+        self.batch_size = batch_size
+        self.momentum = momentum
         
         if activation == 'relu':
             self.activ = relu     
             self.activ_diff = relu_diff
+        elif activation == 'logistic':
+            self.activ = logistic     
+            self.activ_diff = logistic_diff
+
+        
+
+
 
         
     def forward(self, X):
@@ -55,7 +68,7 @@ class MLPBinary():
        
         return hidden_outs_bias, outputs
 
-    def fit(self, X_train, t_train, lr=0.001, epochs=100, X_val=None, t_val=None,  n_epochs_no_update = 2):
+    def fit(self, X_train, t_train, X_val=None, t_val=None):
         """Initialize the weights. Train *epochs* many epochs.
         
         X_train is a NxM matrix, N data points, M features
@@ -63,20 +76,22 @@ class MLPBinary():
         where the values are 0 or 1.
         lr is the learning rate
         """
-        self.lr = lr
-        # create loss to store and inspect
-        self.train_loss = train_loss =[]
-        self.train_acc = []
-        current_epochs_no_update = 0
-        t_start = time.time()
         
+        # create loss to store and inspect
+        self.train_loss = train_loss = []
+        self.train_acc = []
         self.val_loss = val_loss = []
         self.val_acc = val_acc = []
 
+        current_epochs_no_update = 0
+        t_start = time.time()
+        
         # Turn t_train into a column vector, a N*1 matrix:
         T_train = t_train.reshape(-1,1)
         X_train_bias = add_bias(X_train, self.bias)
+        self.N = X_train_bias.shape[0] # number datapoints
 
+        # dimensions of hidden and output layer
         dim_in = X_train.shape[1] 
         dim_out = T_train.shape[1]
         
@@ -89,16 +104,47 @@ class MLPBinary():
             dim_out) * 2 - 1)/np.sqrt(self.dim_hidden)
         
         
-        for e in range(epochs):
+        for e in range(self.epochs):
+
             if time.time()-t_start > 60*2:
                 print("time exceedeed")
                 break
+            
+            if self.solver == 'sgd':
+                # selecting random elements of X_train_bias as batch
+                idx = np.random.randint(0, self.N, self.batch_size)
+                X_batch = X_train[idx, :]
+                T_batch = T_train[idx]
 
+                self.update(X_batch, T_batch)
+            else:
+                self.update(X_train, t_train)
+
+            
+
+            if X_val is not None:
+                z = self.compute_loss(self.predict_probabilities(X_val), t_val)
+                val_loss.append(z)
+                val_acc.append(accuracy(self.predict(X_val), t_val))
+
+                if e > 0 and abs(val_loss[e-1]-val_loss[e]) < self.tol:
+                    current_epochs_no_update += 1
+                    if current_epochs_no_update > self.n_epochs_no_update:
+                        #print("Classifier trained for epochs: ", #self.epochs)
+                        break
+                    
+            self.epochs += 1
+
+
+    def update(self, X, t_train):
+            ''''X unbiased data, T_train gold value'''
+
+            X_bias = add_bias(X, self.bias)
             # One epoch
             # The forward step:
-            hidden_outs, outputs = self.forward(X_train_bias)
+            hidden_outs, outputs = self.forward(X_bias)
             # The delta term on the output node:
-            out_deltas = (outputs - T_train)
+            out_deltas = (outputs - t_train)
             # The delta terms at the output of the hidden layer:
             hiddenout_diffs = out_deltas @ self.weights2.T
             # The deltas at the input to the hidden layer:
@@ -108,31 +154,14 @@ class MLPBinary():
 
             # Update the weights:
             self.weights2 -= self.lr * hidden_outs.T @ out_deltas
-            self.weights1 -= self.lr * X_train_bias.T @ hiddenact_deltas 
+            self.weights1 -= self.lr * X_bias.T @ hiddenact_deltas 
 
             # Loss and accuracy
-            acc = accuracy(self.predict(X_train), t_train)
+            acc = accuracy(self.predict(X), t_train)
             self.train_acc.append(acc)
 
-            loss = self.compute_loss(self.predict_probabilities(X_train), t_train)
+            loss = self.compute_loss(self.predict_probabilities(X), t_train)
             self.train_loss.append(loss)
-
-            if X_val is not None:
-                z = self.compute_loss(self.predict_probabilities(X_val), t_val)
-                val_loss.append(z)
-                val_acc.append(accuracy(self.predict(X_val), t_val))
-
-                if e > 0 and abs(val_loss[e-1]-val_loss[e]) < self.tol:
-                    current_epochs_no_update += 1
-                    if current_epochs_no_update > n_epochs_no_update:
-                        #print("Classifier trained for epochs: ", #self.epochs)
-                        break
-                    
-            self.epochs += 1
-
-
-
-
 
     def compute_loss(self, y, t):
         """
